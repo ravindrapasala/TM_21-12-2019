e502784304cca152a02b697e8eb2cff7f42f582c
dell <dell@192.168.1.2>
2019-12-21 09:49:15 +0530

    Second commit [21/12/2019]

--- src/test/java/com/qa/tests/HRM_Login.java
+++ src/test/java/com/qa/tests/HRM_Login.java
@@ -10,7 +10,7 @@ public class HRM_Login {
 		System.setProperty("webdriver.chrome.driver", "C:\\Users\\dell\\Desktop\\Bala\\Driver\\chromedriver_win32 (4)\\chromedriver.exe");
 		ChromeDriver driver = new ChromeDriver();	
 		driver.get("https://www.testingmasters.com/hrm");
-	
+	    // completed..
 		
 
 	}
