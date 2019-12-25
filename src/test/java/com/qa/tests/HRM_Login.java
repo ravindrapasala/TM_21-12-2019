9a5ce47115a854fb8e3a116f3654f6fb83b1d373
dell <dell@192.168.1.2>
2019-12-25 10:36:52 +0530

    Today Commit...

--- src/test/java/com/qa/tests/HRM_Login.java
+++ src/test/java/com/qa/tests/HRM_Login.java
@@ -1,11 +1,16 @@
 package com.qa.tests;
 
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
 
 
 public class HRM_Login {
 
-	public static void main(String[] args) {
+	@Test
+	
+	public void login()
+	
+	{
 		
 		System.setProperty("webdriver.chrome.driver", "C:\\Users\\dell\\Desktop\\Bala\\Driver\\chromedriver_win32 (4)\\chromedriver.exe");
 		ChromeDriver driver = new ChromeDriver();	
